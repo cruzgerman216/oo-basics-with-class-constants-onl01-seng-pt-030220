4759dce6195aaa46b4dff0f53f6f3e216549f80a
German Cruz <cruzgerman216@yahoo.com>
2020-03-13 20:06:42 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,9 +2,8 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  def initialize(brand)
-    @brand = brand
-  end
+  BRANDS = []
+
 
   def cobble
     self.condition = "new"
